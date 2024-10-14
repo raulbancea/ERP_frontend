59757689ec0b514cff08cd4b98d8b82e09211571
Raul <raulbancea@yahoo.com>
2024-10-14 12:32:39 +0300

    Home page update 2

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,19 +1,38 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handleUploadClick = () => {
+    navigate('/documente');
+  };
+
   return (
     <div className="bg-light p-3" style={{ height: '100vh' }}>
       <button className="btn btn-secondary w-100 mb-3">Analiza date</button>
       <nav>
         <ul className="list-group">
-          <li className="list-group-item">Acasa</li>
-          <li className="list-group-item">Angajati</li>
-          <li className="list-group-item">Documente</li>
-          <li className="list-group-item">Inventar</li>
-          <li className="list-group-item">Tranzactii</li>
+          <li className="list-group-item">
+            <Link to="/" className="btn btn-link">Acasa</Link>
+          </li>
+          <li className="list-group-item">
+            <Link to="/angajati" className="btn btn-link">Angajati</Link>
+          </li>
+          <li className="list-group-item">
+            <Link to="/documente" className="btn btn-link">Documente</Link>
+          </li>
+          <li className="list-group-item">
+            <Link to="/inventar" className="btn btn-link">Inventar</Link>
+          </li>
+          <li className="list-group-item">
+            <Link to="/tranzactii" className="btn btn-link">Tranzactii</Link>
+          </li>
         </ul>
       </nav>
-      <button className="btn btn-primary w-100 mt-3">Incarca document</button>
+      <button className="btn btn-primary w-100 mt-3" onClick={handleUploadClick}>
+        Incarca document
+      </button>
     </div>
   );
 };
