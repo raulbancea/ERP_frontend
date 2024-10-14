23abacfd0e0a346e69591c306f805fb677895e81
Raul <raulbancea@yahoo.com>
2024-10-14 12:15:34 +0300

    Home page update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Sidebar from './components/sidebar';
 import Summary from './components/summary';
 import BankBalances from './components/BankBalances';
 import RecentOperations from './components/RecentOperations';
+import Angajati from './components/Angajati';
+import Documente from './components/Documente';
+import Inventar from './components/Inventar';
+import Tranzactii from './components/Tranzactii';
 import './App.css';
 
 function App() {
   return (
-    <div className="container-fluid padding-top">
-      <div className="row">
-        <div className="col-md-3">
-          <Sidebar />
-        </div>
-
-        <div className="col-md-6">
-          <Summary />
-          <BankBalances />
-        </div>
-
-        <div className="col-md-3">
-          <RecentOperations />
+    <Router>
+      <div className="container-fluid padding-top">
+        <div className="row">
+          <div className="col-md-3">
+            <Sidebar />
+          </div>
+          <div className="col-md-6">
+            <Routes>
+              <Route path="/" element={
+                <>
+                  <Summary />
+                  <BankBalances />
+                </>
+              } />
+              <Route path="/angajati" element={<Angajati />} />
+              <Route path="/documente" element={<Documente />} />
+              <Route path="/inventar" element={<Inventar />} />
+              <Route path="/tranzactii" element={<Tranzactii />} />
+              <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+          </div>
+          <div className="col-md-3">
+            <RecentOperations />
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
